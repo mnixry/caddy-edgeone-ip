c9cb8244755d6a3ed4c63d02e5b3f1553b1d24ee
HexMix <32300164+mnixry@users.noreply.github.com>
2025-10-29 16:59:47 +0800

    Refactor EdgeOneIPRange to improve IP validation and caching logic

--- caddyfile.go
+++ caddyfile.go
@@ -11,7 +11,7 @@ import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
-	lru "github.com/hashicorp/golang-lru/v2/expirable"
+	"github.com/hashicorp/golang-lru/v2/expirable"
 	"github.com/pkg/errors"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
@@ -28,7 +28,7 @@ type EdgeOneIPRange struct {
 	SecretKey   string         `json:"secret_key,omitempty"`
 
 	logger *zap.Logger
-	cache  *lru.LRU[string, netip.Prefix]
+	cache  *expirable.LRU[string, bool]
 	client *teo.Client
 	ctx    caddy.Context
 }
@@ -56,7 +56,7 @@ func (s *EdgeOneIPRange) Provision(ctx caddy.Context) error {
 	if cacheTTL == 0 {
 		cacheTTL = time.Hour
 	}
-	s.cache = lru.NewLRU[string, netip.Prefix](cacheSize, nil, cacheTTL)
+	s.cache = expirable.NewLRU[string, bool](cacheSize, nil, cacheTTL)
 
 	cpf := profile.NewClientProfile()
 	credential := common.NewCredential(s.SecretID, s.SecretKey)
@@ -78,7 +78,7 @@ func (s *EdgeOneIPRange) Provision(ctx caddy.Context) error {
 	return nil
 }
 
-func (s *EdgeOneIPRange) validateIP(request *http.Request) (*netip.Prefix, error) {
+func (s *EdgeOneIPRange) parseRemoteAddr(request *http.Request) (*netip.Addr, error) {
 	host, _, err := net.SplitHostPort(request.RemoteAddr)
 	if err != nil {
 		return nil, errors.Wrap(err, "split host port failed")
@@ -87,15 +87,15 @@ func (s *EdgeOneIPRange) validateIP(request *http.Request) (*netip.Prefix, error
 	if err != nil {
 		return nil, errors.Wrap(err, "parse addr failed")
 	}
-	ipStr := ip.String()
-	if cached, ok := s.cache.Get(ipStr); ok {
-		return &cached, nil
-	}
+	return &ip, nil
+}
+
+func (s *EdgeOneIPRange) validateIP(ip *netip.Addr) (bool, error) {
 	describeRequest := teo.NewDescribeIPRegionRequest()
-	describeRequest.IPs = []*string{&ipStr}
+	describeRequest.IPs = []*string{common.StringPtr(ip.String())}
 	describeResponse, err := s.client.DescribeIPRegion(describeRequest)
 	if err != nil {
-		return nil, errors.Wrap(err, "describe IP region failed")
+		return false, errors.Wrap(err, "describe IP region failed")
 	}
 	valid := slices.ContainsFunc(describeResponse.Response.IPRegionInfo, func(info *teo.IPRegionInfo) bool {
 		if info.IsEdgeOneIP == nil || *info.IsEdgeOneIP != "yes" {
@@ -105,27 +105,46 @@ func (s *EdgeOneIPRange) validateIP(request *http.Request) (*netip.Prefix, error
 		if err != nil {
 			return false
 		}
-		return parsed.Compare(ip) == 0
+		return ip.Compare(parsed) == 0
 	})
-	if !valid {
-		return nil, nil
+	return valid, nil
+}
+
+func (s *EdgeOneIPRange) validateIPCached(request *http.Request) (*netip.Prefix, error) {
+	ip, err := s.parseRemoteAddr(request)
+	if err != nil {
+		return nil, errors.Wrap(err, "parse remote addr failed")
 	}
-	prefix := netip.PrefixFrom(ip, ip.BitLen())
-	s.cache.Add(ipStr, prefix)
-	return &prefix, nil
+	ipStr := ip.String()
+	var validated bool
+	if cached, ok := s.cache.Get(ipStr); ok {
+		validated = cached
+	} else {
+		validated, err = s.validateIP(ip)
+		if err != nil {
+			return nil, errors.Wrap(err, "validate IP failed")
+		}
+		s.cache.Add(ipStr, validated)
+	}
+	if validated {
+		prefix := netip.PrefixFrom(*ip, ip.BitLen())
+		return &prefix, nil
+	}
+	return nil, nil
 }
 
 func (s *EdgeOneIPRange) GetIPRanges(request *http.Request) []netip.Prefix {
-	validated, err := s.validateIP(request)
+	logger := s.logger.With(zap.String("ip", request.RemoteAddr))
+	result, err := s.validateIPCached(request)
 	if err != nil {
-		s.logger.Error("validate IP failed", zap.Error(err), zap.String("ip", request.RemoteAddr))
+		logger.Error("validate IP failed", zap.Error(err))
 		return []netip.Prefix{}
 	}
-	if validated == nil {
-		s.logger.Debug("IP is not valid", zap.String("ip", request.RemoteAddr))
+	if result == nil {
+		logger.Debug("IP is not valid", zap.String("ip", request.RemoteAddr))
 		return []netip.Prefix{}
 	}
-	return []netip.Prefix{*validated}
+	return []netip.Prefix{*result}
 }
 
 // UnmarshalCaddyfile implements caddyfile.Unmarshaler.
